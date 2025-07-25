c511a13aa2e94297d7a07c2bc48ccd4c6b3efaf5
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-07-25 13:52:27 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import { CardDemo } from "./cardDemo";
 
 export default function Home() {
   return (
-    <div className="font-sans  grid items-center justify-items-center min-h-screen ">
+    <div className="font-sans grid items-center justify-items-center min-h-screen ">
       <CardDemo />
     </div>
   );
